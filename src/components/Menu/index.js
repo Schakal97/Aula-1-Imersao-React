4d594092ea8a467a37e2881aba70149e69d13153
Schakal97 <i.vcarneiro7@gmail.com>
2020-08-01 12:50:55 -0300

    Commit

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/logo.png';
 import './Menu.css';
-import ButtonLink from './components/ButtonLink';
 import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
 function Menu() {
-    return(
-        <nav className="Menu">
-        <a href="/">
-        <img className="Logo" src={Logo} alt="AluraFlix Logo" />
-        </a>
-        
-          <Button as="a" className="ButtonLink" href="/">
-              Novo vídeo
-          </Button>
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </Link>
 
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
     </nav>
-
-    );
+  );
 }
 
 export default Menu;
